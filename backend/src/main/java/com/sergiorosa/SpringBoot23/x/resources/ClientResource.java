599b2a98f0c4c2a95a58498f5dc64e971faed049
Sergio Rosa <scarlosjazz@gmail.com>
2021-01-15 17:46:47 -0500

    DTO

--- backend/src/main/java/com/sergiorosa/SpringBoot23/x/resources/ClientResource.java
+++ backend/src/main/java/com/sergiorosa/SpringBoot23/x/resources/ClientResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sergiorosa.SpringBoot23.x.entities.Client;
+import com.sergiorosa.SpringBoot23.x.dto.ClientDTO;
 import com.sergiorosa.SpringBoot23.x.services.ClientService;
 
 @RestController
@@ -19,8 +19,8 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = service.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll(){
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/sergiorosa/SpringBoot23/x/services/ClientService.java
+++ backend/src/main/java/com/sergiorosa/SpringBoot23/x/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.sergiorosa.SpringBoot23.x.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sergiorosa.SpringBoot23.x.dto.ClientDTO;
 import com.sergiorosa.SpringBoot23.x.entities.Client;
 import com.sergiorosa.SpringBoot23.x.repositories.ClientRepository;
 
@@ -17,7 +19,9 @@ public class ClientService {
 	
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
-		return repository.findAll();
+	public List<ClientDTO> findAll(){
+		List<Client> list =  repository.findAll();
+		
+		return  list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 }
