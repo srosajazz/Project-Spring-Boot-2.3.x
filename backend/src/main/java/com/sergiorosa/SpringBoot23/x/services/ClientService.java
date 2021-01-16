95df8c2941eab6aeedf9b8e25bc89f49b4c00b1e
Sergio Rosa <scarlosjazz@gmail.com>
2021-01-15 19:18:29 -0500

    Find by id

--- backend/src/main/java/com/sergiorosa/SpringBoot23/x/services/ClientService.java
+++ backend/src/main/java/com/sergiorosa/SpringBoot23/x/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.sergiorosa.SpringBoot23.x.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,4 +25,25 @@ public class ClientService {
 		
 		return  list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity =  obj.get();
+		return new ClientDTO(entity);
+	
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,1 +1,7 @@
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Mick Smith', '11111111111', 4000.00, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Mick Smith', '222222222222', 6000.00, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Mick Smith', '3333333333333', 14000.00, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Mick Smith', '4444444444444', 5000.00, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
